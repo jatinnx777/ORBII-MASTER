46fb240be2c4a61698159bf7de3119231558867d
Jatin <jaykumar2470f@gmail.com>
2026-06-14 01:44:16 +0530

    ORBII updates

--- src/components/common/MLMapView.tsx
+++ src/components/common/MLMapView.tsx
@@ -305,7 +305,7 @@ export const MLMapView = forwardRef<MLMapViewHandle, Props>(function MLMapView(
       ) : null}
 
       {avatarMarkers.map((m) => (
-        <Marker key={m.id} coordinate={toCoords(m.coordinate)} anchor={{ x: 0.5, y: 1 }}>
+        <Marker key={m.id} coordinate={toCoords(m.coordinate)} anchor="bottom">
           <View style={styles.avatarPin}>
             <View style={styles.avatarRing}>
               {m.photoUri ? (
